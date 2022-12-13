60c8e578e59aae0e62b12b2424ed76add39e95ca
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2022-12-13 17:51:24 +0100

    thingsboard setup script

--- VPS/SMHI/setup_job.sh
+++ VPS/SMHI/setup_job.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
-scriptdir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-ps_dir="${scriptdir}/api_fetch.py"
 
+# Construct path to api_fetch.py script
+script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+script_path="${script_dir}/api_fetch.py"
+
+# Get flag arguments
 while getopts h:t: flag
 do
     case "${flag}" in
@@ -10,18 +13,20 @@ do
     esac
 done
 
+# Prompt host if undefined
 if [ -z "$host" ]
 then 
     echo "Thingsboard host: "
     read host
 fi
 
+# Prompt access token if undefined
 if [ -z "$token" ]
 then 
     echo "Thingsboard access token: "
     read token
 fi
 
-#echo $ps_dir $host $token
-#@hourly /usr/bin/python3 $ps_dir $host $token
-crontab -l | { cat; echo "@hourly /usr/bin/python3 ${ps_dir} ${host} ${token}"; } | crontab -
+# Append api_fetch.py script to crontab
+crontab -l | { cat; echo "@hourly /usr/bin/python3 ${script_path} ${host} ${token}"; } | crontab -
+echo "Hourly cron job added"
---
+++ VPS/Thingsboard/setup.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+echo "Fetching requirements"
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jdk wget
+
+echo "Installing thingsboard service"
+wget https://github.com/thingsboard/thingsboard/releases/download/v3.4.2/thingsboard-3.4.2.deb
+sudo dpkg -i thingsboard-3.4.2.deb
+
+echo "Setting up postgres"
+sudo apt-get install -y postgresql
+sudo service postgresql start
+psql_password='thingsboard'
+psql_user='thingsboard'
+
+sudo -u postgres psql -c "CREATE DATABASE thingsboard;"
+sudo -u postgres psql -c "CREATE USER ${psql_user} WITH PASSWORD '${psql_password}';"
+sudo -u postgres psql -c "GRANT ALL PRIVILIGES ON DATABASE 'thingsboard' to ${psql_user};"
+
+echo "Configuring thingsboard"
+tb_config="# DB Configuration   \nexport DATABASE_TS_TYPE=sql \nexport SPRING_DATASOURCE_URL=jdbc:postgresql://localhost:5432/thingsboard   \nexport SPRING_DATASOURCE_USERNAME=${psql_user}  \nexport SPRING_DATASOURCE_PASSWORD=${psql_password} \n# Specify partitioning size for timestamp key-value storage. Allowed values: DAYS, MONTHS, YEARS, INDEFINITE. \nexport SQL_POSTGRES_TS_KV_PARTITIONING=MONTHS   \n"
+sudo echo -e $tb_config >> /etc/thingsboard/conf/thingsboard.conf
+
+
+while getopts s: flag
+do 
+    case "${flag}" in 
+        s) small='yes';;
+    esac
+done
+
+if [ -n "${small}" ]
+then
+    echo 'export JAVA_OPTS="$JAVA_OPTS -Xms256M -Xmx256M"' >> /etc/thingsboard/conf/thingsboard.conf
+fi
+
+echo "Installing thingsboard"
+sudo /usr/share/thingsboard/bin/install/install.sh --loadDemo
+sudo service thingsboard start
